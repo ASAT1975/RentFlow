bdb700432349f0dc97047c1d5a765b0dac417e9b
ASAT1975 <shaikhahmedtijani2006@gmail.com>
2026-07-25 14:17:37 +0000

    fix: always use Render URL, remove LAN IP detection

--- rentflowfrontend/src/api/config.ts
+++ rentflowfrontend/src/api/config.ts
@@ -1,18 +1,2 @@
-import Constants from "expo-constants";
-
-const PRODUCTION_URL = "https://rentflow-7qo8.onrender.com";
-
-function resolveBaseUrl(): string {
-  if (__DEV__) {
-    const hostUri = Constants.expoConfig?.hostUri;
-    if (hostUri) {
-      const lanIp = hostUri.split(":")[0];
-      return `http://${lanIp}:8080`;
-    }
-    return "http://localhost:8080";
-  }
-  return PRODUCTION_URL;
-}
-
-export const API_BASE_URL = resolveBaseUrl();
+export const API_BASE_URL = "https://rentflow-7qo8.onrender.com";
 export const API_URL = `${API_BASE_URL}/api`;
